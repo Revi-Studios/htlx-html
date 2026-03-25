28fbe8e62daed682bf6b00306600d115c792223e
Robbin472 <revistudios732@gmail.com>
2026-03-25 09:01:16 +0000

    Refactor main function to use JavaScript interop for parsing input

--- main.go
+++ main.go
@@ -1,53 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"htlx/cmd/lexer/lexer"
-	"htlx/cmd/lexer/lexertoken"
 	"htlx/cmd/parser"
-	"os"
-	"strings"
+	"syscall/js"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: <path>")
-		return
+	js.Global().Set("parse", js.FuncOf(func(this js.Value, args []js.Value) any {
+        if len(args) < 1 { return nil }
+        input := args[0].String()
+        return parser.Parse(input)
 	}
-
-	file, err := os.ReadFile(os.Args[1])
-
-	if err != nil {
-		fmt.Println("Error:", fmt.Errorf("reading file: %w", err))
-	}
-
-	r, err := parser.Parse(string(file))
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	fmt.Print(r)
-
-}
-
-func printTokens() {
-	file, err := os.ReadFile(os.Args[1])
-
-	if err != nil {
-		fmt.Println("Error:", fmt.Errorf("reading file: %w", err))
-	}
-
-	l := lexer.NewLexer(string(file))
-
-	go func() {
-		for token := range l.Tokens {
-			fmt.Printf("%v \t\"%v\"\n", token.Type.String(), strings.TrimSpace(token.Value))
-			if token.Type == lexertoken.TOKEN_EOF {
-				close(l.Tokens)
-				return
-			}
-		}
-	}()
-
-	lexer.LexBegin(l)
+	)))
+	select {}
 }
